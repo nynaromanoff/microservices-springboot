e3a1066257895462400425312448083387651e55
Jessica Santos <jel_008@hotmail.com>
2020-10-22 23:38:22 -0300

    first commit

---
+++ hr-worker/src/test/java/com/microservices/hrworker/HrWorkerApplicationTest.java
@@ -0,0 +1,14 @@
+package com.microservices.hrworker;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class HrWorkerApplicationTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}

befd65c530d2fbd4efdeb048d5aab88ef21ae0e7
Jessica Santos <jel_008@hotmail.com>
2020-10-26 10:32:57 -0300

    RestTemplate

---
+++ hr-payroll/src/main/java/com/microservices/hrpayroll/entities/Worker.java
@@ -0,0 +1,81 @@
+package com.microservices.hrpayroll.entities;
+
+import java.io.Serializable;
+
+
+public class Worker implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+	
+	private String nome;
+	
+	private Double dailyIncome;
+
+	public Worker() {
+		
+	}
+
+	public Worker(Long id, String nome, Double dailyInCome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.dailyIncome = dailyInCome;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getDailyIncome() {
+		return dailyIncome;
+	}
+
+	public void setDailyIncome(Double dailyIncome) {
+		this.dailyIncome = dailyIncome;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Worker other = (Worker) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+	
+	
+	
+
+}

87d42acfaf62790c0f262a6bacb9c2813cf833a6
Jessica Santos <jel_008@hotmail.com>
2020-10-26 11:38:01 -0300

    Feign Client

--- hr-payroll/src/main/java/com/microservices/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/microservices/hrpayroll/HrPayrollApplication.java
@@ -2,9 +2,9 @@ package com.microservices.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-//@EnableEurekaClient
+@EnableFeignClients
 @SpringBootApplication
 public class HrPayrollApplication {
 
--- hr-payroll/src/main/java/com/microservices/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/microservices/hrpayroll/services/PaymentService.java
@@ -1,31 +1,22 @@
 package com.microservices.hrpayroll.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.microservices.hrpayroll.entities.Payment;
 import com.microservices.hrpayroll.entities.Worker;
+import com.microservices.hrpayroll.feignclientes.WorkerFeignClient;
 
 @Service
 public class PaymentService {
 	
-	@Value("${hr-worker.host}")
-	private String workerHost;
 	
 	@Autowired
-	private RestTemplate restTemplate;
-
+	private WorkerFeignClient workerFeignClient;
+	
 	public Payment getPayment(long workerId, int days) {
 		
-		Map<String, String> uriVariable = new HashMap<>();
-		uriVariable.put("id", ""+workerId);
-		
-		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariable);
+		Worker worker = workerFeignClient.findById(workerId).getBody();
 		return new Payment(worker.getNome(), worker.getDailyIncome(), days);
 	}
 }
